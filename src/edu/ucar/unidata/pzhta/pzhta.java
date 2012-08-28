cc533d9509a44d5978d85c77a1d23d43a1f433ef
Sean Arms <sarms@flip.unidata.ucar.edu>
2012-08-28 14:03:22 -0600

    First commit of pzhta

---
+++ src/edu/ucar/unidata/pzhta/pzhta.java
@@ -0,0 +1,29 @@
+package edu.ucar.unidata.pzhta;
+
+import ucar.nc2.dataset.NetcdfDataset;
+import ucar.nc2.ncml.NcMLReader;
+import ucar.nc2.NetcdfFile;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sarms
+ */
+public class pzhta {
+    public static void main(String args[]){
+        String ncml_filename = "/Users/sarms/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/test.ncml";
+        String fileout_name = "/Users/sarms/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/pzhta_test.nc";
+        System.out.println( "*** Reading NCML");
+        try{
+            NetcdfDataset ncd = NcMLReader.readNcML (ncml_filename, null);
+            System.out.println( "*** Writing netCDF file");
+            NetcdfFile ncdnew = ucar.nc2.FileWriter.writeToFile(ncd, fileout_name, true);
+            ncd.close();
+            ncdnew.close();
+            System.out.println( "*** Done");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
