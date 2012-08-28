55bb7f838c2270ab438864037f0bc8edb1c260ac
Sean Arms <sarms@unidata.ucar.edu>
2012-08-28 15:45:37 -0600

    fixing intellij files so that proper files are ignored

--- src/edu/ucar/unidata/pzhta/pzhta.java
+++ src/edu/ucar/unidata/pzhta/pzhta.java
@@ -5,6 +5,8 @@ import ucar.nc2.ncml.NcMLReader;
 import ucar.nc2.NetcdfFile;
 
 import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,11 +14,11 @@ import java.io.IOException;
  */
 public class pzhta {
     public static void main(String args[]){
-        String ncml_filename = "/Users/sarms/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/test.ncml";
-        String fileout_name = "/Users/sarms/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/pzhta_test.nc";
+        String ncml_filename = "file:///Users/lesserwhirls/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/test.ncml";
+        String fileout_name = "/Users/lesserwhirls/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/pzhta_test.nc";
         System.out.println( "*** Reading NCML");
         try{
-            NetcdfDataset ncd = NcMLReader.readNcML (ncml_filename, null);
+            NetcdfDataset ncd = NcMLReader.readNcML(ncml_filename, null);
             System.out.println( "*** Writing netCDF file");
             NetcdfFile ncdnew = ucar.nc2.FileWriter.writeToFile(ncd, fileout_name, true);
             ncd.close();
