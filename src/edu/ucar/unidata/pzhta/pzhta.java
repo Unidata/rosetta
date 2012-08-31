93b6518d3bd9a15aa7a85b9796ab59d99b7488ea
Sean Arms <sarms@unidata.ucar.edu>
2012-08-30 20:53:38 -0600

    now everythign works in intellij. Playing with netcdf-java in pzhta.java to list varaibles in the NetcdfDataset variable.

--- src/edu/ucar/unidata/pzhta/pzhta.java
+++ src/edu/ucar/unidata/pzhta/pzhta.java
@@ -5,6 +5,8 @@ import ucar.nc2.ncml.NcMLReader;
 import ucar.nc2.NetcdfFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Iterator;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,6 +19,15 @@ public class pzhta {
         System.out.println( "*** Reading NCML");
         try{
             NetcdfDataset ncd = NcMLReader.readNcML(ncml_filename, null);
+            List vars = ncd.getVariables();
+            // Use iterator to display contents of al
+            System.out.print("Variables in file:\n");
+            Iterator itr = vars.iterator();
+            while(itr.hasNext()) {
+                Object element = itr.next();
+                System.out.print("  " + element + "\n");
+            }
+            System.out.println();
             System.out.println( "*** Writing netCDF file");
             NetcdfFile ncdnew = ucar.nc2.FileWriter.writeToFile(ncd, fileout_name, true);
             ncd.close();
