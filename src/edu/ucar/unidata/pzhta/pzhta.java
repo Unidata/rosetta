f58c640a89fa3ea5324aeb3dffa46955f32b9302
Sean Arms <sarms@unidata.ucar.edu>
2012-09-10 10:19:16 -0600

    updated .gitignore and changed print statements for testing.

--- src/edu/ucar/unidata/pzhta/pzhta.java
+++ src/edu/ucar/unidata/pzhta/pzhta.java
@@ -3,6 +3,7 @@ package edu.ucar.unidata.pzhta;
 import ucar.nc2.dataset.NetcdfDataset;
 import ucar.nc2.ncml.NcMLReader;
 import ucar.nc2.NetcdfFile;
+import ucar.nc2.dataset.VariableDS;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,16 +17,22 @@ public class pzhta {
     public static void main(String[] args) {
         String ncml_filename = "file:///Users/lesserwhirls/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/test.ncml";
         String fileout_name = "/Users/lesserwhirls/dev/unidata/pzhta/src/edu/ucar/unidata/pzhta/test/pzhta_test.nc";
-        System.out.println( "*** Reading NCML");
+        System.out.println( "*** Reading NCML\n");
         try{
             NetcdfDataset ncd = NcMLReader.readNcML(ncml_filename, null);
-            List vars = ncd.getVariables();
-            // Use iterator to display contents of al
-            System.out.print("Variables in file:\n");
-            Iterator itr = vars.iterator();
+            List globalAttributes = ncd.getGlobalAttributes();
+            // Use iterator to display all Global Attributes
+            System.out.print("Global Attributes in file:\n");
+            Iterator itr = globalAttributes.iterator();
             while(itr.hasNext()) {
                 Object element = itr.next();
                 System.out.print("  " + element + "\n");
+            }
+            System.out.print("\n");
+            System.out.print("Variables in file:\n");
+            List vars = ncd.getVariables();
+            for (int var = 0; var < vars.size(); var = var + 1){
+                System.out.print("  " + vars.get(var)  + "\n");
             }
             System.out.println();
             System.out.println( "*** Writing netCDF file");
