71a78735143459fdaa437007cf79873d9f4fe9e0
Jennifer Oxelson <oxelson@mochrie.unidata.ucar.edu>
2012-09-10 10:52:32 -0600

    adding spring backend stuff

---
+++ src/edu/ucar/unidata/pzhta/controller/AsciiParserController.java
@@ -0,0 +1,105 @@
+package edu.ucar.unidata.pzhta.controller;
+
+import org.grlea.log.SimpleLogger;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.ucar.unidata.pzhta.domain.AsciiFile;
+
+/**
+ * Controller to parse ASCII file data.
+ *
+ */
+
+@Controller
+public class AsciiParserController {
+
+    private static final SimpleLogger log = new SimpleLogger(AsciiParserController.class);
+
+    @RequestMapping(value="/parse", method=RequestMethod.POST)
+    @ResponseBody
+    public String parseFile(AsciiFile file, BindingResult result) {
+        String filePath = System.getProperty("java.io.tmpdir") + "/" + file.getUniqueId() + "/" + file.getFileName();
+        StringBuffer sBuffer = new StringBuffer();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String sCurrentLine;            
+            int lineCount = 0;
+            int delimiterRunningTotal = 0;
+            boolean dataLine = false;
+            List <String> delimiterList = file.getDelimiterList(); 
+            String selectedDelimiter = "";      
+            if (!file.getDelimiterList().isEmpty()) {
+                selectedDelimiter = delimiterList.get(0);  
+            }             
+            while ((sCurrentLine = reader.readLine()) != null) {
+                if (file.getHeaderLineNumberList().isEmpty()) {
+                    sBuffer.append(sCurrentLine + "\n");     
+                } else {
+                    if (file.getDelimiterList().isEmpty()) {
+                        log.error("ASCII file header lines are present, but no delimiters are specified.");
+                        return null;
+                    } else {
+                        if (file.getHeaderLineNumberList().contains(new Integer(lineCount).toString())) {
+                            sBuffer.append(sCurrentLine + "\n");    
+                        } else {             
+                            Iterator<String> delimiterIterator = delimiterList.iterator();
+                            while (delimiterIterator.hasNext()) {  
+                                String delimiter = delimiterIterator.next();
+                                int delimiterCount = StringUtils.countMatches(sCurrentLine, delimiter);
+                                if (!dataLine) {
+                                    delimiterRunningTotal = delimiterCount;
+                                    dataLine = true;
+                                } else {       
+                                    if (delimiterRunningTotal != delimiterCount) {
+                                        log.error("ASCII file line of data contains an irregular delimiter count at line number: " + new Integer(lineCount).toString() + " for delimiter: " + delimiter);
+                                        return null;
+                                    }
+                                }                               
+                            }
+                            if (delimiterList.size() != 1) {
+                                String[] delimiters = (String[])delimiterList.toArray(new String[delimiterList.size()]);
+                                for(int i = 1; i< delimiters.length; i++){ 
+		                     String updatedLineData = sCurrentLine.replaceAll(delimiters[i],  selectedDelimiter);
+                                     sBuffer.append(updatedLineData + "\n");   
+                                }                            
+                            } else {
+                                sBuffer.append(sCurrentLine + "\n");   
+                            }
+                        }
+                    }
+                }
+                lineCount++;
+            }   
+            if (!file.getDelimiterList().isEmpty()) {
+                selectedDelimiter = selectedDelimiter + "\n";
+            }
+            return selectedDelimiter + sBuffer.toString();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            return null;
+	}
+    }
+}
---
+++ src/edu/ucar/unidata/pzhta/controller/FileUploadController.java
@@ -0,0 +1,130 @@
+package edu.ucar.unidata.pzhta.controller;
+
+import org.grlea.log.SimpleLogger;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.ucar.unidata.pzhta.domain.UploadedFile;
+
+/**
+ * Controller to handle file uploads.
+ *
+ */
+
+
+@Controller
+public class FileUploadController implements HandlerExceptionResolver {
+
+    private static final SimpleLogger log = new SimpleLogger(FileUploadController.class);
+
+
+/*
+    @RequestMapping(method=RequestMethod.GET)
+    public String showForm(ModelMap model){
+        UploadForm form = new UploadForm();
+        model.addAttribute("FORM", form);
+        return "upload";
+    }
+*/
+
+    /*
+     * Returns the local (unique, alphanumeric) file name of the 
+     * uploaded ASCII file as it is found on disk.
+     * 
+     * @param file      The UploadedFile form backing object containing the file.
+     * @param request   The HttpServletRequest with which to glean the client IP address.
+     * @return          A String of the local file name for the ASCII file. 
+     */
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    @ResponseBody
+    public String processUpload(UploadedFile file, HttpServletRequest request) {
+        FileOutputStream outputStream = null;
+        String uniqueId = createUniqueId(request);
+        String filePath = System.getProperty("java.io.tmpdir") + "/" + uniqueId;
+        try {
+            File localFileDir = new File(filePath);
+            if (!localFileDir.exists()) localFileDir.mkdir();
+            outputStream = new FileOutputStream(new File(filePath + "/" + file.getFileName()));
+            outputStream.write(file.getFile().getFileItem().get());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            log.error("A file upload error has occurred: " + e.getMessage());
+            return null;
+        }
+        return uniqueId;                 
+    }
+
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception exception) {
+        Map<Object, Object> model = new HashMap<Object, Object>();
+        if (exception instanceof MaxUploadSizeExceededException){ 
+           log.error("File size should be less then "+ ((MaxUploadSizeExceededException)exception).getMaxUploadSize()+" byte.");
+        } else{
+           log.error("An error has occurred: " + exception.getMessage());
+        }        
+        return null;
+    }
+
+
+    /*
+     * Attempts to get the client IP address from the request.
+     * 
+     * @param request     The HttpServletRequest.
+     * @return            The client's IP address.
+     */
+    private String getIpAddress(HttpServletRequest request) {
+        String ipAddress = null;
+        if (request.getRemoteAddr() != null) {
+            ipAddress = request.getRemoteAddr();
+            ipAddress = StringUtils.deleteWhitespace(ipAddress);            
+            ipAddress = StringUtils.trimToNull(ipAddress);
+            ipAddress = StringUtils.lowerCase(ipAddress);  
+            ipAddress = StringUtils.replaceChars(ipAddress, ".", "");
+        }
+        return ipAddress;
+    }
+
+
+    /*
+     * Creates a unique id for the file name from the clients IP address and the date.
+     * 
+     * @param request     The HttpServletRequest.
+     * @return            The unique file name id.
+     */
+    private String createUniqueId(HttpServletRequest request) {
+
+        String id = new Integer(new Date().hashCode()).toString();
+        String ipAddress = getIpAddress(request);
+        if (ipAddress != null) {
+            id = ipAddress + id;
+        } else {
+            id = new Integer(new Random().nextInt()).toString() + id;
+        }
+        return id;
+    }
+
+
+}
