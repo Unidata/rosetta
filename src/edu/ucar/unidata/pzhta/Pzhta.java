684cc0f13ae420f56b13a4a038af72f4851a0f1c
Sean Arms <sarms@unidata.ucar.edu>
2012-09-11 16:44:14 -0600

    write bogus values from List of List to netCDF file

--- src/edu/ucar/unidata/pzhta/Pzhta.java
+++ src/edu/ucar/unidata/pzhta/Pzhta.java
@@ -5,7 +5,14 @@ import org.grlea.log.SimpleLogger;
 import ucar.nc2.dataset.NetcdfDataset;
 import ucar.nc2.ncml.NcMLReader;
 import ucar.nc2.NetcdfFile;
-import ucar.nc2.dataset.VariableDS;
+import ucar.nc2.Variable;
+import ucar.nc2.NetcdfFileWriteable;
+import ucar.nc2.Attribute;
+import ucar.nc2.Dimension;
+
+import ucar.ma2.InvalidRangeException;
+import ucar.ma2.DataType;
+import ucar.ma2.ArrayFloat;
 
 import java.io.IOException;
 import java.util.List;
@@ -49,6 +56,40 @@ public class Pzhta {
             ncd.close();
             ncdnew.close();
             log.info( "*** Done");
+
+            // open netCDF file
+            NetcdfFileWriteable ncfile = NetcdfFileWriteable.openExisting(fileout_name, true);
+            vars = ncfile.getVariables();
+            // get time dim
+            Dimension timeDim = ncfile.findDimension("time");
+            for (int var = 0; var < vars.size(); var = var + 1){
+                Variable tmp_var = (Variable) vars.get(var);
+                String varName = tmp_var.getName();
+                Attribute attr = tmp_var.findAttribute("_colNum");
+                DataType dt = tmp_var.getDataType();
+                //String thing = attr.getStringValue();
+                if (attr != null) {
+                    int varIndex = Integer.parseInt(attr.getStringValue());
+                    int len = outerList.size();
+
+                    //if (dt.toString().equals("float")) {
+                    ArrayFloat.D1 vals = new ArrayFloat.D1(timeDim.getLength());
+                    //} else {
+                    for (int i = 0; i < timeDim.getLength(); i++) {
+                        List row = (List) outerList.get(i);
+                        vals.set(i,Float.valueOf((String) row.get(varIndex)).floatValue());
+                        System.out.print("  " + (String) row.get(varIndex) + "\n");
+                    }
+                    try {
+                        ncfile.write(varName, vals);
+                    } catch (ucar.ma2.InvalidRangeException e) {
+                        log.error(e.getStackTrace().toString());
+                        return false;
+                    }
+                }
+            }
+            ncfile.close();
+
             File file = new File(fileout_name);
             if(file.exists()) { 
                 return true;
@@ -60,5 +101,18 @@ public class Pzhta {
             log.error(e.getStackTrace().toString());
             return false;
         }
+    }
+    public static void main(String[] args) {
+        Pzhta pz = new Pzhta();
+        ArrayList<List<String>> outerList = new ArrayList<List<String>>();
+        for (int j=0; j<10; j++) {
+            ArrayList<String> innerList = new ArrayList<String>();
+            for (int i=0; i<11; i++) {
+                innerList.add(Integer.toString((i+j)*i));
+            }
+            outerList.add(innerList);
+        }
+        pz.convert(" ", " ", outerList);
+
     }
 }
