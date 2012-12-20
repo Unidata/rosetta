a631ae1c91678e31e602c0553921ef744fada647
Sean Arms <sarms@unidata.ucar.edu>
2012-12-20 09:21:08 -0700

    cleaned up ncml output - made it *purdy*

--- src/edu/ucar/unidata/pzhta/controller/AsciiParserController.java
+++ src/edu/ucar/unidata/pzhta/controller/AsciiParserController.java
@@ -26,10 +26,7 @@ import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -447,10 +444,12 @@ public class AsciiParserController {
 
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
             DOMSource source = new DOMSource(doc);
             File ncmlFile = new File(ncmlFilePath);
             StreamResult result = new StreamResult(ncmlFile);
- 
+
             transformer.transform(source, result);
 
 
