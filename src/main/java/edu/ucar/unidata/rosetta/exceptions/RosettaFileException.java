bfcdc60119324d74adbe460558438fa8adfcacad
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-06-03 19:56:59 -0600

    new exception dealing with file IO

---
+++ src/main/java/edu/ucar/unidata/rosetta/exceptions/RosettaFileException.java
@@ -0,0 +1,15 @@
+package edu.ucar.unidata.rosetta.exceptions;
+
+/**
+ * Exception dealing with Rosetta File IO issues.
+ *
+ * @author oxelson@ucar.edu
+ */
+public class RosettaFileException extends Exception {
+
+    public RosettaFileException() {}
+
+    public RosettaFileException(String message) {
+        super(message);
+    }
+}
