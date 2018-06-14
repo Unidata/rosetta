25a96092385e7bdbf52a31e535423932a69f30c6
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-06-14 15:38:15 -0600

    added spring security user authentication and access control

---
+++ src/main/java/edu/ucar/unidata/rosetta/service/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,41 @@
+package edu.ucar.unidata.rosetta.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+/**
+ * Service class used to handle a successful user authentication.
+ *
+ * @author oxelson@ucar.edu
+ */
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    /**
+     * Called when a user has been successfully authenticated.
+     *
+     * @param httpServletRequest  The request which caused the successful authentication.
+     * @param httpServletResponse The response.
+     * @param authentication  Authentication object which was created during the authentication process.
+     * @throws IOException  If unable to send the redirect in the response.
+     * @throws ServletException  If errors occur during the servlet request/response cycle.
+     */
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                                        HttpServletResponse httpServletResponse,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        // Set our response to OK status.
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+
+        // Since we have created our custom success handler, its up to us
+        // to decide where we will redirect the user after successfully login.
+        httpServletResponse.sendRedirect("wizard");
+    }
+}
+
