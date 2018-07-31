2a30d0121f2437172a31c63ba01d50484925aa82
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-07-31 16:13:34 -0600

    added DAO classes for metadat profile resource

---
+++ src/main/java/edu/ucar/unidata/rosetta/repository/resources/MetadataProfileDao.java
@@ -0,0 +1,34 @@
+package edu.ucar.unidata.rosetta.repository.resources;
+
+import edu.ucar.unidata.rosetta.domain.resources.MetadataProfile;
+import java.util.List;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataRetrievalFailureException;
+
+/**
+ * The data access object representing a metadata profile.
+ *
+ * @author oxelson@ucar.edu
+ */
+public interface MetadataProfileDao {
+
+  /**
+   * Looks up and retrieves a list of persisted MetadataProfile objects.
+   *
+   * @return A List of all persisted metadata profiles.
+   * @throws DataAccessException If unable to retrieve persisted metadata profile.
+   */
+  public List<MetadataProfile> getMetadataProfiles() throws DataAccessException;
+
+  /**
+   * Looks up and retrieves a persisted MetadataProfile object using the provided name.
+   *
+   * @param name The name of the metadata profile to retrieve.
+   * @return The MetadataProfile object matching the provided name.
+   * @throws DataRetrievalFailureException If unable to retrieve persisted metadata profile.
+   */
+  public MetadataProfile lookupMetadataProfileByName(String name)
+      throws DataRetrievalFailureException;
+
+
+}
