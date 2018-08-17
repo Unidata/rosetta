1f0befda63d2507955b1726189984bc1cad80f12
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-08-17 16:00:04 -0600

    added more recent metadata profiles

--- src/main/webapp/WEB-INF/classes/resources/MpsProfilesRosetta/MpsSqlQuery_OutputRosettaProfile-ETUFF.sql
+++ src/main/webapp/WEB-INF/classes/resources/MpsProfilesRosetta/MpsSqlQuery_OutputRosettaProfile-ETUFF.sql
@@ -1,12 +1,12 @@
 SELECT
   public.vw_get_profilestructureattributes.profileshortname AS "metadataProfileName",
   public.vw_get_profilestructureattributes.profileversion AS "metadataProfileVersion",
-  public.vw_get_profilestructureattributes.attributename AS attibutename,
+  public.vw_get_profilestructureattributes.attributename AS "attibuteName",
   replace(public.vw_get_profilestructureattributes.attributename, '_', ' ') AS "displayName",
-  public.vw_get_profilestructureattributes.attributedescription AS description,
-  public.vw_get_profilestructureattributes.attributevalues AS exampleValues,
+  public.vw_get_profilestructureattributes.attributedescription AS "description",
+  public.vw_get_profilestructureattributes.attributevalues AS "exampleValues",
   trim(both public.vw_get_profilestructureattributes.fmt_structuretype) AS "metadataType",
-  trim(both public.vw_get_profilestructureattributes.fmt_structurename_short) AS "metadataTypeStructureName",
+  trim(both public.vw_get_profilestructureattributes.fmt_structurename_short) AS "metadataTypeStructure",
   trim(both public.vw_get_profilestructureattributes."group") AS "metadataGroup",
   public.vw_get_profilestructureattributes.attributetype AS "metadataValueType",
   public.vw_get_profilestructureattributes.attributenecessity AS "complianceLevel"
