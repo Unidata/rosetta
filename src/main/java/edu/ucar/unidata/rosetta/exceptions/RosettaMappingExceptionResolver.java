21ece75dc6776f51dfd4c86eed5be586f27f44dd
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2018-10-03 13:18:36 -0600

    custom exception extending SimpleMappingExceptionResolver

---
+++ src/main/java/edu/ucar/unidata/rosetta/exceptions/RosettaMappingExceptionResolver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2012-2018 University Corporation for Atmospheric Research/Unidata
+ * See LICENSE for license information.
+ */
+
+package edu.ucar.unidata.rosetta.exceptions;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+public class RosettaMappingExceptionResolver extends SimpleMappingExceptionResolver  {
+
+    private static final Logger logger = Logger.getLogger(RosettaMappingExceptionResolver.class);
+   // private static Log log = LogFactory.getLog(LoggingExceptionResolver.class);
+
+    public RosettaMappingExceptionResolver() {
+        super();
+    }
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request,
+                                         HttpServletResponse response, Object handler, Exception exception) {
+        logger.error(getStackTrace(exception));
+        return super.resolveException(request, response, handler, exception);
+    }
+
+
+    public String getStackTrace(Throwable t) {
+        StringWriter stringWritter = new StringWriter();
+        PrintWriter printWritter = new PrintWriter(stringWritter, true);
+        t.printStackTrace(printWritter);
+        printWritter.flush();
+        stringWritter.flush();
+        return stringWritter.toString();
+    }
+
+
+}
