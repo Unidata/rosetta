585ffa8f3613f8405d04073587b9ca902e0fb1e5
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2020-03-03 16:07:26 -0700

    validator for data collected in the CfTypeController

--- src/main/java/edu/ucar/unidata/rosetta/service/validators/wizard/CfTypeValidator.java
+++ src/main/java/edu/ucar/unidata/rosetta/service/validators/wizard/CfTypeValidator.java
@@ -8,24 +8,26 @@ package edu.ucar.unidata.rosetta.service.validators.wizard;
 import edu.ucar.unidata.rosetta.domain.wizard.WizardData;
 import edu.ucar.unidata.rosetta.service.ResourceManager;
 import edu.ucar.unidata.rosetta.service.validators.CommonValidator;
-
 import java.util.List;
 import javax.annotation.Resource;
-
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 /**
- * Validator class that corresponds to the data collected via the CfTypeController. ID is not set yet. The user must
- * provide either the platform or CF type value. Required and non-required user input data that is not empty is
- * evaluated for naughty strings and chars (see CommonValidator), and passing that is confirmed to be legitimate by
- * comparing with the persisted approved resources. Populates the Errors object if validation fails.
+ * Validator class that corresponds to the data collected via the CfTypeController. ID is not set yet.
+ * The user must provide either the platform or CF type value.  Required and non-required user input data that is
+ * not empty is evaluated for naughty strings and chars (see CommonValidator), and passing that is confirmed to be
+ * legitimate by comparing with the persisted approved resources. Populates the Errors object if validation fails.
  */
 @Component("CfTypeValidator")
 public class CfTypeValidator extends CommonValidator implements Validator {
+
+  private static final Logger logger = LogManager.getLogger(WizardDataValidator.class);
 
   @Resource(name = "resourceManager")
   private ResourceManager resourceManager;
@@ -50,7 +52,6 @@ public class CfTypeValidator extends CommonValidator implements Validator {
   @Override
   public void validate(@NotNull Object obj, @NotNull Errors errors) {
     WizardData data = (WizardData) obj;
-    logger.info(data.toString());
     String cfType = data.getCfType();
     String platform = data.getPlatform();
     String metadataProfile = data.getMetadataProfile();
@@ -60,30 +61,27 @@ public class CfTypeValidator extends CommonValidator implements Validator {
 
     // No errors so far, validate the actual data.
     if (!errors.hasErrors()) {
-      if (!validateNotEmpty(cfType)) { // CF type provided.
 
+      if (!StringUtils.isBlank(cfType)) { // CF type provided.
         // Check for dubious input.
-        findDubiousInput(cfType, errors);
-        if (!validateNotEmpty(metadataProfile)) { // metadataProfile value can be null
-          findDubiousInput(metadataProfile, errors);
-        }
+        validateInput("cfType", cfType, errors);
+        validateInput("metadataProfile", metadataProfile, errors);
 
         // CF type data contains nothing naughty, confirm user-provided data is a legit CF type.
-        if (!errors.hasGlobalErrors()) {
+        if (errors.getFieldErrorCount("cfType") > 0) {
           validateCfType(cfType, errors);
         }
 
         // Metadata profile data contains nothing naughty, confirm user-provided data is a legit metadata profile.
-        if (!errors.hasGlobalErrors() && !validateNotEmpty(metadataProfile)) {
+        if (errors.getFieldErrorCount("metadataProfile") > 0) {
           validateMetadataProfile(metadataProfile, errors);
         }
-
-      } else { // Platform provided.
+      } else {  // Platform provided.
         // Check for dubious input.
-        findDubiousInput(platform, errors);
+        validateInput("platform", platform, errors);
 
         // Platform contains nothing naughty, confirm user-provided data is a legit platform.
-        if (!errors.hasGlobalErrors()) {
+        if (errors.getFieldErrorCount("platform") > 0) {
           validatePlatform(platform, errors);
         }
       }
@@ -101,8 +99,8 @@ public class CfTypeValidator extends CommonValidator implements Validator {
   private void validateCfType(String cfType, Errors errors) {
     // Get the list of persisted & blessed CF types names.
     List<String> blessedCfTypeNames = resourceManager.getCfTypeNames();
-    if (!blessedCfTypeNames.contains(StringUtils.replaceChars(cfType, "_", " "))) {
-      logError("Invalid CF type value submitted: " + cfType);
+    if (!blessedCfTypeNames.contains(cfType)) {
+      logger.info("VALIDATION ERROR: Invalid CF type value submitted: " + cfType);
       errors.reject("cfType.invalid");
     }
   }
@@ -117,13 +115,12 @@ public class CfTypeValidator extends CommonValidator implements Validator {
    */
   private void validateMetadataProfile(String metadataProfile, Errors errors) {
     String[] userProvidedMetadataProfiles = StringUtils.split(metadataProfile, ",");
-    logger.info(metadataProfile);
     // Get the list of persisted & blessed metadata profile names.
     List<String> blessedMetadataProfileNames = resourceManager.getMetadataProfileNames();
-    for (String profile : userProvidedMetadataProfiles) {
-      // Check if the user-provided metadata profile is one of the approved profiles.
-      if (!blessedMetadataProfileNames.contains(StringUtils.replaceChars(profile, "_", " "))) {
-        logError("Invalid metadata profile value submitted: " + profile);
+    for (String name : userProvidedMetadataProfiles) {
+      // Check if the user-provided metadata profile name is one of the approved profile names.
+      if (!blessedMetadataProfileNames.contains(name)) {
+        logger.info("VALIDATION ERROR: Invalid metadata profile value submitted: " + name);
         errors.reject("metadataProfile.invalid");
         return;
       }
@@ -140,8 +137,8 @@ public class CfTypeValidator extends CommonValidator implements Validator {
   private void validatePlatform(String platform, Errors errors) {
     // Get the list of persisted & blessed platform names.
     List<String> blessedPlatformNames = resourceManager.getPlatformNames();
-    if (!blessedPlatformNames.contains(StringUtils.replaceChars(platform, "_", " "))) {
-      logError("Invalid platform value submitted by: " + platform);
+    if (!blessedPlatformNames.contains(platform)) {
+      logger.info("VALIDATION ERROR: Invalid platform value submitted: " + platform);
       errors.reject("platform.invalid");
     }
   }
@@ -149,16 +146,15 @@ public class CfTypeValidator extends CommonValidator implements Validator {
   /**
    * Checks to make sure either the platform or CF type was provided by the user. (Both cannot be empty). The platform
    * is associated with a CF type and will be used to determine the CF type value at a later stage in the program.
-   * <p>
    * Populates the Errors object if validation fails.
    *
    * @param cfType   The user-provided CF type.
-   * @param platform The user-provided platform.
+   * @param platform The user-provided  platform.
    * @param errors   Object to hold validation errors.
    */
   private void validateRequiredData(String cfType, String platform, Errors errors) {
-    if (validateNotEmpty(cfType) && validateNotEmpty(platform)) {
-      logError("Required data CF type or platform not provided");
+    if (StringUtils.isBlank(cfType) && StringUtils.isBlank(platform)) {
+      logger.info("VALIDATION ERROR: required data CF type or platform not provided.");
       errors.reject("cfType.missingData");
     }
   }
