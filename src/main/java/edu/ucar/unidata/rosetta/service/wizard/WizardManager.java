35220f9f9b3195c275e3e69f12559f842b4560fb
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2020-03-03 16:07:26 -0700

    changed methods to package access.  split apart processCfType method into 2 different overloaded methods

--- src/main/java/edu/ucar/unidata/rosetta/service/wizard/WizardManager.java
+++ src/main/java/edu/ucar/unidata/rosetta/service/wizard/WizardManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2020 University Corporation for Atmospheric Research/Unidata.
+ * Copyright (c) 2012-2019 University Corporation for Atmospheric Research/Unidata.
  * See LICENSE for license information.
  */
 
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Service for handling data collected from the wizard.
  */
+@SuppressWarnings("SpellCheckingInspection")
 public interface WizardManager {
 
   String getTemplateFile(String id);
@@ -25,7 +26,7 @@ public interface WizardManager {
    * @throws RosettaFileException If unable to create the template file.
    * @throws RosettaDataException If unable to parse data file with delimiter.
    */
-  public String convertToNetcdf(String id) throws RosettaFileException, RosettaDataException;
+  String convertToNetcdf(String id) throws RosettaFileException, RosettaDataException;
 
   /**
    * Determines whether the custom file attributes step needs to be visited in the wizard.
@@ -33,7 +34,7 @@ public interface WizardManager {
    * @param id The ID corresponding to the persisted data needed to make this determination.
    * @return true if custom file attributes step needs to be visited; otherwise false;
    */
-  public boolean customFileAttributesStep(String id);
+  boolean customFileAttributesStep(String id);
 
   /**
    * Looks up and retrieves persisted wizard data using the given ID.
@@ -41,7 +42,7 @@ public interface WizardManager {
    * @param id The ID corresponding to the data to retrieve.
    * @return The persisted wizard data.
    */
-  public WizardData lookupPersistedWizardDataById(String id);
+  WizardData lookupPersistedWizardDataById(String id);
 
   /**
    * Retrieves the data file from disk and parses it by line, converting it into a JSON string. Used in the wizard for
@@ -51,17 +52,26 @@ public interface WizardManager {
    * @return A JSON string of the file data parsed by line.
    * @throws RosettaFileException For any file I/O or JSON conversions problems.
    */
-  public String parseDataFileByLine(String id) throws RosettaFileException;
+  String parseDataFileByLine(String id) throws RosettaFileException;
 
   /**
-   * Processes the data collected from the wizard for the CF type step.
+   * Processes the data collected from the wizard for the CF type step. Persisted data corresponding to the ID already
+   * exists. This prior persisted data is collected and updated with the new data contained WizardData object.
    *
    * @param id The unique ID corresponding to already persisted data (may be null).
    * @param wizardData The WizardData object containing user-submitted CF type information.
-   * @param request HttpServletRequest used to make unique IDs for new data.
-   * @throws RosettaDataException If unable to lookup the metadata profile.
+   * @throws RosettaDataException If unable to lookup/persist wizard data.
    */
-  public void processCfType(String id, WizardData wizardData, HttpServletRequest request)
+  void processCfType(String id, WizardData wizardData) throws RosettaDataException, RosettaFileException;
+
+  /**
+   * Processes the data collected from the wizard for the CF type step for the first time.
+   *
+   * @param wizardData The WizardData object containing user-submitted CF type information.
+   * @param request HttpServletRequest used to make unique IDs for new data.
+   * @throws RosettaDataException If unable to lookup/persist wizard data.
+   */
+  void processCfType(WizardData wizardData, HttpServletRequest request)
       throws RosettaDataException, RosettaFileException;
 
   /**
@@ -70,27 +80,27 @@ public interface WizardManager {
    * @param id The unique ID corresponding to already persisted data.
    * @param wizardData The WizardData containing custom file type attribute data.
    */
-  public void processCustomFileTypeAttributes(String id, WizardData wizardData);
+  void processCustomFileTypeAttributes(String id, WizardData wizardData);
 
   /**
-   * Processes the data submitted by the user containing global metadata information. Since this
-   * is the final step of collecting data in the wizard, the uploaded data file is converted to
-   * netCDF format in preparation for user download.
+   * Processes the data submitted by the user containing global metadata information. Since this is the final step of
+   * collecting data in the wizard, the uploaded data file is converted to netCDF format in preparation for user
+   * download.
    *
    * @param id The unique ID corresponding to already persisted data.
    * @param wizardData The WizardData containing the global metadata.
    * @throws RosettaDataException If unable to populate the metadata object.
    */
-  public void processGlobalMetadata(String id, WizardData wizardData) throws RosettaDataException;
+  void processGlobalMetadata(String id, WizardData wizardData) throws RosettaDataException;
 
   /**
-   * Determines the next step in the wizard based the user specified data file type. This method is called when there
-   * is a divergence of possible routes through the wizard.
+   * Determines the next step in the wizard based the user specified data file type. This method is called when there is
+   * a divergence of possible routes through the wizard.
    *
    * @param id The unique ID corresponding to already persisted data.
    * @return The next step to redirect the user to in the wizard.
    */
-  public String processNextStep(String id);
+  String processNextStep(String id);
 
   /**
    * Determines the previous step in the wizard based the user specified data file type. This method is called when
@@ -99,7 +109,7 @@ public interface WizardManager {
    * @param id The unique ID corresponding to already persisted data.
    * @return The previous step to redirect the user to in the wizard.
    */
-  public String processPreviousStep(String id);
+  String processPreviousStep(String id);
 
   /**
    * Processes the data submitted by the user containing variable metadata information.
@@ -107,12 +117,12 @@ public interface WizardManager {
    * @param id The unique ID corresponding to already persisted data.
    * @param wizardData The WizardData containing variable metadata information.
    */
-  public void processVariableMetadata(String id, WizardData wizardData);
+  void processVariableMetadata(String id, WizardData wizardData);
 
   /**
    * Updates persisted wizard data with the information in the provided WizardData object.
    *
    * @param wizardData The updated wizard data.
    */
-  public void updatePersistedWizardData(WizardData wizardData);
+  void updatePersistedWizardData(WizardData wizardData);
 }
