716f07c0b9ea1cdbe10d94b29f5e93d4b8883c90
Jennifer Oxelson <oxelson@ucar.edu>
2020-06-22 17:45:40 -0600

    logging method. combined known dubious input check

--- src/main/java/edu/ucar/unidata/rosetta/service/validators/CommonValidator.java
+++ src/main/java/edu/ucar/unidata/rosetta/service/validators/CommonValidator.java
@@ -6,6 +6,8 @@
 package edu.ucar.unidata.rosetta.service.validators;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.validation.Errors;
 
 /**
@@ -14,40 +16,27 @@ import org.springframework.validation.Errors;
 @SuppressWarnings("SpellCheckingInspection")
 public abstract class CommonValidator {
 
-  private static final String[] NAUGHTY_STRINGS = {"<script>", "../", "svg", "javascript", "::", "&quot;",
+  protected static final Logger logger = LogManager.getLogger(CommonValidator.class);
+
+  private static final String[] NAUGHTY = {"<", ">", "`", "^", "|", "}", "{", "script", "../", "svg", "javascript", "::", "&quot;",
       "fromcharCode", "%3", "$#", "alert(", ".js", ".source", "\\", "scriptlet", ".css", "binding:", ".htc", "vbscript",
       "mocha:", "livescript:", "base64", "\00", "xss:", "%77", "0x", "IS NULL;", "1;", "; --", "1=1"};
-  private static final String[] NAUGHTY_CHARS = {"<", ">", "`", "^", "|", "}", "{"};
   private String ipAddress;
 
   /**
-   * Checks if the provided string contains anything the NAUGHTY_STRINGS list.
-   *
-   * @param itemToCheck The string to check.
-   * @return The item in NAUGHTY_STRINGS that matches the provided string; otherwise null.
+   * Checks if the provided string contains any known, dubious strings or chars contained in the NAUGHTY list.
+   * <p>
+   * Populates the Errors object if validation fails.
+   *  @param input The user input for the form form field.
+   * @param errors Object to store validation errors.
    */
-  private String checkForNaughtyStrings(String itemToCheck) {
-    for (String item : NAUGHTY_STRINGS) {
-      if (StringUtils.contains(StringUtils.lowerCase(itemToCheck), item)) {
-        return item;
+  protected void findDubiousInput(String input, Errors errors) {
+    for (String naughty : NAUGHTY) {
+      if (StringUtils.contains(input, naughty)) {
+        logError("Bad input submitted: " + naughty);
+        errors.reject("badInputData");
       }
     }
-    return null;
-  }
-
-  /**
-   * Checks if the provided string contains anything the NAUGHTY_CHARS list.
-   *
-   * @param itemToCheck The string to check.
-   * @return The item in NAUGHTY_CHARS that matches the provided string; otherwise null.
-   */
-  private String checkForNaughtyChars(String itemToCheck) {
-    for (String item : NAUGHTY_CHARS) {
-      if (StringUtils.contains(itemToCheck, item)) {
-        return item;
-      }
-    }
-    return null;
   }
 
   /**
@@ -56,8 +45,17 @@ public abstract class CommonValidator {
    *
    * @return the IP address.
    */
-  public String getIpAddress() {
+  private String getIpAddress() {
     return ipAddress;
+  }
+
+  /**
+   * Records the validation error message in the logs.
+   *
+   * @param message The message to log.
+   */
+  protected void logError(String message) {
+    logger.info("VALIDATION ERROR  [" + getIpAddress() + "]  " + message);
   }
 
   /**
@@ -71,26 +69,6 @@ public abstract class CommonValidator {
   }
 
   /**
-   * Checks if the provided string contains any known, dubious strings or chars.
-   *
-   * @param field The form field to be validated.
-   * @param input The user input for the form form field.
-   * @param errors Object to store validation errors.
-   */
-  protected void validateInput(String field, String input, Errors errors) {
-    String badChar = checkForNaughtyChars(input);
-    if (badChar != null) {
-      errors.rejectValue(field, "badInputData", "Bad value submitted: " + badChar);
-    }
-    if (errors.getFieldErrorCount(field) > 0) {
-      String badString = checkForNaughtyStrings(input);
-      if (badString != null) {
-        errors.rejectValue(field, "badInputData", "Bad value submitted: " + badString);
-      }
-    }
-  }
-
-  /**
    * Checks if provided string is empty (""), null or whitespace only.
    * <p>
    * The StringTrimmerEditor property editor is registered with the DataBinder in the controllers, which trims strings
@@ -99,6 +77,7 @@ public abstract class CommonValidator {
    * behavior is removed.
    *
    * @param input The user input for the form form field.
+   * @return eturn true if the CharSequence is null, empty or whitespace only
    */
   protected boolean validateNotEmpty(String input) {
     return StringUtils.isBlank(input);
